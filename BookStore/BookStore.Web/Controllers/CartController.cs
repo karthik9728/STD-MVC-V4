d887f42a9ad1b096684d41ebac482e25fa9f5546
Karthik <56579449+karthik9728@users.noreply.github.com>
2024-01-13 09:34:22 +0530

    Cart Index Page

---
+++ BookStore/BookStore.Web/Controllers/CartController.cs
@@ -0,0 +1,27 @@
+﻿using BookStore.Services;
+using BookStore.Web.Filter;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Web.Controllers
+{
+    public class CartController : Controller
+    {
+
+        private IBookStoreService _bookService;
+        private IAuthService _authService;
+
+        public CartController(IBookStoreService bookService, IAuthService authService)
+        {
+            _bookService = bookService;
+            _authService = authService;
+        }
+
+
+        [CheckSession("userId")]
+        public IActionResult Index()
+        {
+            var carts = _bookService.GetCartDetailsByUserId((int)HttpContext.Session.GetInt32("userId"));
+            return View(carts);
+        }
+    }
+}
